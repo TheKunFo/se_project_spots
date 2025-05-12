8ea3c50bdad9c92895ed1dfe36cabc43f71352c3
TheKunFo <thekunfo@icloud.com>
2025-05-11 22:17:13 -0500

    project9

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -9,6 +9,7 @@ class Api {
   }
 
   editUserInfo({ name, about }) {
+    console.log(name, about);
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: this._headers,
